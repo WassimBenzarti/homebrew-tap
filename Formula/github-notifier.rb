de72f133caa0e4937fca5cb764706a7df60354be
Wassim Benzarti <20246795+WassimBenzarti@users.noreply.github.com>
2024-09-16 21:23:28 +0200

    github-notifier 0.0.2

--- Formula/github-notifier.rb
+++ Formula/github-notifier.rb
@@ -1,31 +1,25 @@
 class GithubNotifier < Formula
-  desc "Boost your productivity with instant alerts for PR activities. Get notified about review requests, new reviews, and check statuses. Minimize wait times and stay informed about your team's GitHub activity in real-time"
-  homepage ""
+  desc "Get notified about review requests, new reviews, and check statuses"
+  homepage "https://github.com/WassimBenzarti/github-notifier"
+  url "https://github.com/WassimBenzarti/github-notifier/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "d1aabc827b58b0722d4230a6c0593f6156b03a6104e3bbb633822ee68fff4a50"
   license "MIT"
+  head "https://github.com/WassimBenzarti/github-notifier.git", branch: "main"
 
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/WassimBenzarti/github-notifier/releases/download/v0.0.1/github-notifier-darwin-arm64.gz"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/WassimBenzarti/github-notifier/releases/download/v0.0.1/github-notifier-darwin-amd64.gz"
-    end
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
-  if OS.linux?
-    if Hardware::CPU.arm?
-      url "https://github.com/WassimBenzarti/github-notifier/releases/download/v0.0.1/github-notifier-linux-arm64.gz"
-    end
-    if Hardware::CPU.intel?
-      url "https://github.com/WassimBenzarti/github-notifier/releases/download/v0.0.1/github-notifier-linux-amd64.gz"
-    end
-    
-  end
+
+  depends_on "go" => :build
 
   def install
-    bin.install "github-notifier"
+    system "go", "build", "-C", "cli/", *std_go_args(ldflags: "-s -w")
+    bin.install "bin/github-notifier"
+    generate_completions_from_executable(bin/"github-notifier", "completion")
   end
 
   test do
-    system bin/"github-notifier"
+    assert_equal version.to_s, shell_output("#{bin}/github-notifier version").strip
   end
 end
